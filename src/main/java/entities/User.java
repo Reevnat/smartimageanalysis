35b91931103a280ce6df0d845dd0ed7f8f17ceb0
Tanveer <mtrahmn@gmail.com>
2018-03-28 15:40:27 +0100

    changed sql connection in pom to 5.1.39, changed google vision version to 1.23 from 1.14. Changed User.java to a entity class (@entity) and linked vairables to respective columns in mysql table and added password getter and setter.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,8 +1,37 @@
 package entities;
 
-public class User {
+import java.util.List;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+import javax.persistence.Embeddable;
+
+@Entity
+@Table(name = "users")
+@SuppressWarnings("serial")
+public class User implements Serializable{
+    @Id
+    @GeneratedValue
+    @Column(name = "id", length = 11 )
     int id;
+
+    @Column(name = "email")
     String email;
+
+    String password;
+    
     public int getId() {
         return id;
     }
@@ -18,4 +47,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() { return password;}
+
+    public void setPassword(String password){this.password = password;}
 }
--- target/appengine-staging/WEB-INF/jsp/home.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-    <h1>Welcome SpingMVC4 Home Page! <br> Tanveer</h1>
-</body>
-</html>
